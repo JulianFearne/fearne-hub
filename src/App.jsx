2fa5797a03506f2d0e53c3a96ff234326b08fcb1
JulianFearne <julian.fearne@outlook.com>
2026-07-20 10:51:47 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -38,6 +38,7 @@ export default function App() {
                 <Recipes />
               </ProtectedRoute>
             }
+          />
           <Route
             path="/import"
             element={
